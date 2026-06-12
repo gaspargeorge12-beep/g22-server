10d6b68bb5459c97beed0682ae64ea4ab75659ab
gaspargeorge12-beep <gaspargeorge12@gmail.com>
2026-06-12 14:38:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -33,4 +33,4 @@ def parse_stl(data):
     return {'faces':[{'faceIdx':0,'vertices':verts,'normals':norms,'area':0,'nTriangles':n}],'source':'stl'}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
